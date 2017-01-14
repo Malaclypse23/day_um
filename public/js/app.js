0172d1c9223f6c32d32dd289364b898f353dfaf3
Thomas Spanner <malaclypse17@web.de>
2017-01-14 17:35:03 +0100

    angular basics

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,63 @@
+$(function() {
+	lightbox.option({
+		'resizeDuration': 200,
+		'wrapAround': true,
+		'alwaysShowNavOnTouchDevices': true,
+		'albumLabel':	"Image %1 of %2",
+	});
+});
+
+
+angular.module('ShopApp', ['ngRoute'])
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+      .when('/shop.html', {
+        templateUrl: '/shop.html',
+        controller: 'ProductController'
+      });
+	}])
+	.controller('ProductController', function ProductController($scope, $window) {
+	  $scope.products = [
+	    { name: 'Red Hand 1', type: "Print", price: '9 EUR', quantity: 1, filename: "img/big/redhand1.jpg" },
+			{ name: 'Red Hand 2', type: "Print", price: '9 EUR', quantity: 1, filename: "img/big/redhand2.jpg" },
+			{ name: 'Red Hand 3', type: "Print", price: '9 EUR', quantity: 1, filename: "img/big/redhand3.jpg" },
+	    { name: 'Birds 1', type: "T-Shirt", price: '19 EUR', quantity: 1, filename: "img/big/birds1.jpg", size: ['M', 'L', 'XL'] },
+			{ name: 'Birds 3', type: "T-Shirt", price: '19 EUR', quantity: 1, filename: "img/big/birds3.jpg", size: ['M', 'L', 'XL'] }
+	  ];
+	})
+	.controller('CartController', function CartController($scope, $window) {
+		$scope.cart = [
+	    { name: 'Red Hand 1 Print', price: '9 EUR', quantity: 2, total: '18 EUR' }
+	  ];
+	});
+	/*
+	.controller('AddController', function AddController($scope, $window) {
+		add-to-cart() {
+			console.log(added!);
+		}
+
+	});
+*/
+
 function clearLocalStorage() {
 	//if (localStorage) {
 		var s = { 'lang': 'en', 'debug': false };
-		localStorage.setItem('settings',  JSON.stringify(s));		
+		localStorage.setItem('settings',  JSON.stringify(s));
 	//}
 }
 
 $(function() {
 
-
 	var settings = null;
 	if (localStorage) {
-		try { 
+		try {
 			settings = JSON.parse(localStorage.getItem('settings'));
 		}
 		catch(e) {
     		if (e.name == "NS_ERROR_FILE_CORRUPTED") settings = { 'lang': 'en', 'debug': false };
     	}
     }
-	
+
 	if (settings == null) {
 		var cart = {
 			item: "Product 1",
@@ -25,63 +65,10 @@ $(function() {
 			qty: 2
 		};
 
-		settings = { 'lang': 'en', 'debug': false, 'cart':  JSON.stringify(cart)}; 
+		settings = { 'lang': 'en', 'debug': false, 'cart':  JSON.stringify(cart)};
 	}
 
-	localStorage.setItem('settings', JSON.stringify(settings));	
-
-
-	$.Shop = function( element ) {
-		this.$element = $(element); // top-level element
-		this.init();
-	};
-
-	$.Shop.prototype = {
-		init: function() {
-			// initializes properties and methods
-			this.cartPrefix = "winery-"; // prefix string to be prepended to the cart's name in session storage
-			this.cartName = this.cartPrefix + "cart"; // cart's name in session storage
-			this.shippingRates = this.cartPrefix + "shipping-rates"; // shipping rates key in session storage
-			this.total = this.cartPrefix + "total"; // total key in the session storage
-			this.storage = sessionStorage; // shortcut to sessionStorage object
-
-
-			this.$formAddToCart = this.$element.find( "form.add-to-cart" ); // forms for adding items to the cart
-			this.$formCart = this.$element.find( "#shopping-cart" ); // Shopping cart form
-			this.$checkoutCart = this.$element.find( "#checkout-cart" ); // checkout form cart
-			this.$checkoutOrderForm = this.$element.find( "#checkout-order-form" ); // checkout user details form
-			this.$shipping = this.$element.find( "#sshipping" ); // element that displays the shipping rates
-			this.$subTotal = this.$element.find( "#stotal" ); // element that displays the subtotal charges
-			this.$shoppingCartActions = this.$element.find( "#shopping-cart-actions" ); // cart actions links
-			this.$updateCartBtn = this.$shoppingCartActions.find( "#update-cart" ); // update cart button
-			this.$emptyCartBtn = this.$shoppingCartActions.find( "#empty-cart" ); // empty cart button
-			this.$userDetails = this.$element.find( "#user-details-content" ); // element that displays the user's information
-			this.$paypalForm = this.$element.find( "#paypal-form" ); // PayPal form
-
-
-			this.currency = "&euro;"; // HTML entity of the currency to be displayed in layout
-			this.currencyString = "€"; // currency symbol as text string
-			this.paypalCurrency = "EUR"; // PayPal's currency code
-			this.paypalBusinessEmail = "yourbusiness@email.com"; // your PayPal Business account email address
-			this.paypalURL = "https://www.sandbox.paypal.com/cgi-bin/webscr"; // URL of the PayPal form
-
-			// object containing patterns for form validation
-			this.requiredFields = {
-				expression: {
-					//value: /^([w-.]+)@((?:[w]+.)+)([a-z]){2,4}$/
-				},
-
-				str: {
-					value: ""
-				}
-
-			};
-		}
-	};
-
-	$(function() {
-		var shop = new $.Shop( "#root" ); // object's instance
-	});
+	localStorage.setItem('settings', JSON.stringify(settings));
 
 	if (settings.lang == 'en') {
 		$('.deutsch').each(function() {
@@ -96,31 +83,23 @@ $(function() {
 
 	$("#language").on('click', function() {
 		var settings;
-		try { 
+		try {
 			settings = JSON.parse(localStorage.getItem('settings'));
 		} catch(e) {
     		if (e.name == "NS_ERROR_FILE_CORRUPTED") {
     			settings = { 'lang': 'en', 'debug': false };
     		}
     	}
-    	
+
 		if (localStorage) {
 			if (settings.lang == "en") {
-				var s = { 'lang': 'de', 'debug': true };	
+				var s = { 'lang': 'de', 'debug': true };
 				localStorage.setItem('settings',  JSON.stringify(s));
-			} else { 
-				var s = { 'lang': 'en', 'debug': true };	
-				localStorage.setItem('settings', JSON.stringify(s));	
+			} else {
+				var s = { 'lang': 'en', 'debug': true };
+				localStorage.setItem('settings', JSON.stringify(s));
 			}
-			location.reload();	
+			location.reload();
 		}
 	});
-
-    lightbox.option({
-      'resizeDuration': 200,
-      'wrapAround': true,
-      'alwaysShowNavOnTouchDevices': true,
-      'albumLabel':	"Image %1 of %2",
-    });
-
 });
--- server.js
+++ server.js
@@ -1,21 +1,15 @@
 const express = require('express');
 const app = express();
-
 app.use(express.static('public'));
 
+
+app.listen(80, () => {
+  console.log('listening on 80')
+})
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 })
-
-app.get('/beards.html', (req, res) => {
-  res.sendFile(__dirname + '/beards.html');
-})
-
-app.get('/birds.html', (req, res) => {
-  res.sendFile(__dirname + '/birds.html');
-})
-
 app.get('/contact.html', (req, res) => {
   res.sendFile(__dirname + '/contact.html');
 })
@@ -25,27 +19,11 @@ app.get('/contact.html', (req, res) => {
 To connect using the mongo shell:
 mongo ds163738.mlab.com:63738/stina -u <dbuser> -p <dbpassword>
 To connect using a driver via the standard MongoDB URI (what's this?):
-
 mongodb://<stina>:<newworld1>@ds163738.mlab.com:63738/stina
 mongod version: 3.2.11 (MMAPv1)
 */
-
-const MongoClient = require('mongodb').MongoClient
-var db
-
-MongoClient.connect('mongodb://<stina>:<newworld1>@ds163738.mlab.com:63738/stina', (err, database) => {
-	//if (err) return console.log(err)
-
-	db = database
-  	app.listen(80, () => {
-    	console.log('listening on 80')
-  	})
-
-	console.log("mongo nasen");
-
-
-
-
-})
-
-
+//const MongoClient = require('mongodb').MongoClient
+var MongoClient = require('mongodb').MongoClient;
+//var Db = require('mongodb').Db;
+//var ObjectId = require('mongodb').ObjectID;
+var url = 'mongodb://<tomahawk>:<stina>@ds163738.mlab.com:63738/stina';
