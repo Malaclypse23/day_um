529a9c603bc6fc3b99225c23bba8a525657d2374
Malaclypse23 <malaclypse17@web.de>
2017-03-19 00:05:27 +0100

    no shop, margins header, cookie language works

--- public/js/app.js
+++ public/js/app.js
@@ -1,60 +1,33 @@
 var lang;
 
-function select_language(language) {
+function select_language(language, isChange) {
 	$("[lang]").each(function () {
         if ($(this).attr("lang") == language)
             $(this).show();
         else
             $(this).hide();
     });
-    /*
-    var settings = $.cookie("settings");
-    if (language == 'en') {
-		$('header select').val("de");
-		settings = { 'lang': 'de', 'debug': false };
-	} else {
-		$('header select').val("en");
-		settings = { 'lang': 'en', 'debug': false };
-	}
-
-    $.cookie("settings", JSON.stringify(settings));
-
-    if (language == 'en') {
-		$('header select').val("de");
+  
+    if (language == 'de' && isChange) {
+		$('header select option[value="de"]').attr("selected", "selected");
 		$.cookie("lang", 'de');
-	} else {
-		$('header select').val("en");
+		lang = "de";
+	} else if (language == 'en' && isChange) {
+		$('header select option[value="en"]').attr("selected", "selected");
 		$.cookie("lang", 'en');
-	}
-	*/
-	//$('header select option[value=language]');
-
-	if (language == 'en') {
-		$.cookie("lang", 'de');
-		$("header select option[value='de']");
-	} else {
-		$.cookie("lang", 'en');
-		$("header select option[value='en']");
+		lang = "en";
 	}
 }
 
-
 $(function() {
-
-	// disable right-click SAVE AS!
-	//document.addEventListener('contextmenu', event => event.preventDefault());
-	var lang = $.cookie("lang");
-	if (lang == undefined || lang == null || lang == 'undefined') {
+	lang = $.cookie("lang");
+	if (lang == undefined) {
 		lang = 'en';
 	} else if (lang == 'de') {
-		$("header select option[value='de']");	
+		$('header select option[value="de"]').attr("selected", "selected");
 	}
 	
-	//lang = $('header select option:selected').text(); // TODO
-	select_language(lang);
-	
-	//$.cookie("settings", JSON.stringify(settings));
-	//$.cookie("lang", lang);
+	select_language(lang, false);
 
 	lightbox.option({
 		'resizeDuration': 200,
@@ -66,20 +39,6 @@ $(function() {
 	/*
 	var cart = { item: "Product 1", price: 19.00, qty: 2 };
 	'cart':  JSON.stringify(cart)
-
 	$.cookie("settings", JSON.stringify(settings));
 	*/
-
-	/*
-	$("#language").on('click', function() {
-		if (settings.lang == "en") {
-			var s = { 'lang': 'de', 'debug': true };
-			$.cookie("settings", JSON.stringify(s));
-		} else {
-			var s = { 'lang': 'en', 'debug': true };
-			$.cookie("settings", JSON.stringify(s));
-		}
-		location.reload();
-	});
-*/
 });
