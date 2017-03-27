865dfe69f6804b2f6be5dfb4a2e252b4d0293a68
Malaclypse23 <malaclypse17@web.de>
2017-03-27 20:10:55 +0100

    deleted img subfolder

--- gulpfile.js
+++ gulpfile.js
@@ -38,7 +38,7 @@ gulp.task('styles', function() {
       sass: 'sass/',
       image: 'public/img'
     }))
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest(cssDir));
 });
--- public/js/app.js
+++ public/js/app.js
@@ -42,9 +42,8 @@ function select_language(language, isChange) {
 $(function() {
 	$('.social-media-icons li:last-of-type').on('click', function() {
 		$('.fa-plus').toggleClass('fa-minus');
-		//$('.main-menu ul').delay(80).fadeToggle();
-		$('.main-menu ul').fadeToggle();
-		
+		$('.main-menu ul').delay(80).fadeToggle();
+		//$('.main-menu ul').fadeToggle();
 	});
 
 	$('#reply').change(function() {
