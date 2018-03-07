5202e051b61039cc2879a927fc8a885c9e32292c
Malaclypse23 <malaclypse17@web.de>
2018-03-07 13:50:30 +0000

    big year

--- public/js/app.js
+++ public/js/app.js
@@ -91,6 +91,21 @@ $(function() {
 
 	select_language(lang, false);
 
+	// detect scroll on index / contact
+	var mq = window.matchMedia( "(min-width: 641px)" );
+	if (mq.matches) {
+		var lastScrollTop = 0;
+		$(window).scroll(function(event) {
+		   var st = $(this).scrollTop();
+		   if (st > lastScrollTop) {
+		       $('#root.is-text .main-menu').fadeOut();
+		   } else if ($(window).scrollTop() < 100) {
+		      $('#root.is-text .main-menu').fadeIn();
+		   }
+		   lastScrollTop = st;
+		});
+	}	
+
 	$(".fade > div").on("swipeleft", swipeleftHandler);
  	$(".fade > div").on("swiperight", swiperightHandler);
  	$(".fade > div").on("click", getCurrentImage);
@@ -114,6 +129,7 @@ $(function() {
 		isStopped = true;
 		var $next = $('.show-image').next();
 		$('.fade div').removeClass("show-image");
+		
 		if (!$next.length && !$next.hasClass('caption')) {
 			$('.fade > div:first-of-type').addClass("show-image");
 		} else if (!$next.hasClass('caption')) {
